94574078105f943acbd5c75f2ae803ccc65961c2
aavd1971 <aavd1971@gmail.com>
2015-03-03 21:24:18 +0300

    add 6 files

---
+++ data.js
@@ -0,0 +1,46 @@
+    data = {
+        dano: {
+            sl1: {
+                top: 1,
+                bottom: 3
+            },
+            sl2: {
+                top: 2,
+                bottom: 7
+            }
+        },
+        solution: {
+            stage1: {
+                sl1: {
+                    mn: 7,
+                    titleTop: 'введите числитель первой дроби',
+                    titleBottom: 'введите знаменатель первой дроби'
+                },
+                sl2: {
+                    mn: 3,
+                    titleTop: 'введите числитель второй дроби',
+                    titleBottom: 'введите знаменатель второй дроби'
+                }
+            },
+            stage2: {
+                sl1: {
+                    top: 7,
+                    bottom: 21,
+                    titleTop: 'умножьте 1 на 7',
+                    titleBottom: 'умножьте 3 на 7'
+                },
+                sl2: {
+                    top: 6,
+                    bottom: 21,
+                    titleTop: 'умножьте 2 на 3',
+                    titleBottom: 'умножьте 7 на 3'
+                }
+            },
+            stage3: {
+                top: 13,
+                bottom: 21,
+                titleTop: 'сложите числители',
+                titleBottom: 'запишите знаменатели'
+            }
+        }
+    }
